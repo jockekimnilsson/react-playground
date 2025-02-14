f787a76600c91e9daca6bfa33e6e8fe478b3df30
Joakim <jockekimnilsson@gmail.com>
2025-02-14 08:36:38 +0100

    update main.yml

--- my-react-app/src/main.tsx
+++ my-react-app/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import CounterApp from './Counter.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <CounterApp />
   </React.StrictMode>
 )
